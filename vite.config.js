a6d9f6c6800f697847e67ff18c02b025257ef36d
HAMALAWY\Lenovo <te7a.12345@gmail.com>
2024-10-23 21:08:41 +0300

    first3 commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/E-learning/",
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
 });
